a895441e740d137ebce24875c64d96bd5fc5b50d
Zhen Yang <wwxyzd@hotmail.com>
2020-06-25 09:22:10 -0700

    finished assignment for lesson07

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 
+# for many to many InlineModelAdmin
 class MyInline(admin.TabularInline):
     model = Category.posts.through
 
@@ -9,7 +10,7 @@ class PostAdmin(admin.ModelAdmin):
     inlines = [MyInline, ]
 
 class CategoryAdmin(admin.ModelAdmin):
-    #inlines = [MyInline, ]
+    inlines = [MyInline, ]
     exclude = ('posts',)
 
 # Register your models here.
